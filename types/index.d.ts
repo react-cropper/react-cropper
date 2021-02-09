d507326931c2b15b2c439d413a5e0f37f25b7886
Septs <github@septs.pw>
2021-02-09 09:17:20 +0800

    refactor: redesign typing

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,18 +1,33 @@
-import React from 'react';
-import Cropper from 'cropperjs';
-interface ReactCropperProps
-    extends Cropper.Options,
-        Omit<React.HTMLProps<HTMLImageElement>, 'data' | 'ref' | 'crossOrigin'> {
-    crossOrigin?: '' | 'anonymous' | 'use-credentials' | undefined;
-    on?: (eventName: string, callback: () => void | Promise<void>) => void | Promise<void>;
+import type Instance from 'cropperjs';
+import type React from 'react';
+
+interface EventMap {
+    crop: Instance.CropEvent;
+    cropend: Instance.CropEndEvent;
+    cropmove: Instance.CropMoveEvent;
+    cropstart: Instance.CropStartEvent;
+    ready: Instance.ReadyEvent;
+    zoom: Instance.ZoomEvent;
+}
+
+interface EventHandler {
+    <K extends keyof EventMap>(eventName: K, callback: (event: EventMap[K]) => void): Promise<void>;
+}
+
+interface Props extends Cropper.Options, Omit<React.HTMLProps<HTMLImageElement>, 'data' | 'ref' | 'crossOrigin'> {
+    enable?: boolean;
     scaleX?: number;
     scaleY?: number;
-    enable?: boolean;
     zoomTo?: number;
     rotateTo?: number;
-    onInitialized?: (instance: Cropper) => void | Promise<void>;
+    crossOrigin?: '' | 'anonymous' | 'use-credentials';
+    on?: EventHandler;
+    onInitialized?: (instance: Instance) => void;
 }
-declare const ReactCropper: React.ForwardRefExoticComponent<ReactCropperProps & React.RefAttributes<HTMLImageElement>>;
+
+declare const ReactCropper: React.ForwardRefExoticComponent<Props & React.RefAttributes<HTMLImageElement>>;
+
+export {Instance, EventHandler};
 export {ReactCropper as Cropper};
 export {ReactCropper as default};
-export {ReactCropperProps};
+export {Props as ReactCropperProps};
